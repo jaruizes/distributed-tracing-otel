09849041dfa07406ad4f5e3cfee4d8a161bc6314
Jose Albertio Ruiz Casarrubios <e_jrui05@mercadona.com>
2023-01-16 18:11:11 +0100

    Added initial version of services

---
+++ b-service/src/test/java/com/jaruiz/examples/observability/ExampleResourceIT.java
@@ -0,0 +1,7 @@
+package com.jaruiz.examples.observability;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
