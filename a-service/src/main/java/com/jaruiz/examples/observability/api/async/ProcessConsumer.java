7102440c0ebdac17e7c4e5617c591abda8eb0e9f
Jose Albertio Ruiz Casarrubios <e_jrui05@mercadona.com>
2023-01-19 00:21:57 +0100

    Fixed IO execution

--- a-service/src/main/java/com/jaruiz/examples/observability/api/async/ProcessConsumer.java
+++ a-service/src/main/java/com/jaruiz/examples/observability/api/async/ProcessConsumer.java
@@ -6,6 +6,8 @@ import javax.inject.Inject;
 import com.jaruiz.examples.observability.api.async.dto.ProcessDataMessage;
 import com.jaruiz.examples.observability.business.model.ProcessData;
 import com.jaruiz.examples.observability.business.ports.AServiceBusinessPort;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import io.smallrye.common.annotation.Blocking;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
@@ -13,7 +15,9 @@ public class ProcessConsumer {
 
     @Inject AServiceBusinessPort aService;
 
+    @Blocking
     @Incoming("topic-d")
+    @WithSpan("consume-A")
     public void consume(ProcessDataMessage processDataMessage) {
         aService.saveProcess(messageToBM(processDataMessage));
     }
